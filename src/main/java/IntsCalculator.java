1f0725bcd33bb7df70f9520a8ac258b1edc11eb0
Sergey Kubetskiy <kubetskiy@gmail.com>
2022-10-13 17:19:55 +0300

    Initial commit

---
+++ src/main/java/IntsCalculator.java
@@ -0,0 +1,32 @@
+public class IntsCalculator implements Ints {
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        Calculator calc = new Calculator();
+        return (int )calc.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM)
+                .result();
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        Calculator calc = new Calculator();
+        return (int )calc.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT)
+                .result();
+    }
+
+    @Override
+    public int pow(int arg0, int arg1) {
+        Calculator calc = new Calculator();
+        return (int )calc.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.POW)
+                .result();
+    }
+}
